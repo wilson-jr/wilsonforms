02fb9dee66fa1d5797c3337cc91bb2a92c84b116
extassyy <47561561+wilson-jr@users.noreply.github.com>
2022-05-13 17:21:32 +0400

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="text-center text-lg-start" style="background-color: transparent; z-index: 2000; d-none">
-  <!-- Copyright -->
-  <div class="text-center p-3">
-    © 2022 Copyright:
-    <a class="text-dark" href="https://discordapp.com/users/292689366843785216">Wilson</a>
-  </div>
-  <!-- Copyright -->
-</footer>
