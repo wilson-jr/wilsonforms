86e00a4a75ac03af800aeecaefee2c2f8db89549
extassyy <47561561+wilson-jr@users.noreply.github.com>
2022-05-13 19:57:38 +0400

    Add files via upload

---
+++ src/js/csureport.js
@@ -0,0 +1,10 @@
+function csureport() {
+  var writeCode = document.getElementById('code');
+
+  var backhomeForm = document.getElementById('code');
+
+  document.getElementById("popup-1").classList.toggle("active");
+  writeCode.innerHTML = "<code> \
+  asd "+document.getElementById('sender').value+" \
+  </code>";
+}
