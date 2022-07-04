0a20407e778748202e493e8b1ef460d4d9973e89
extassyy <47561561+wilson-jr@users.noreply.github.com>
2022-07-04 23:35:44 +0400

    Add files via upload

---
+++ pages/lssd/iab/notification.js
@@ -0,0 +1,31 @@
+function notification() {
+  var writeCode = document.getElementById('code');
+
+  document.getElementById("exampleModal").classList.toggle("active");
+  writeCode.innerHTML = "<code> <br>\
+  [textarea][box=gray][imgalign=left]https://i.ibb.co/9tjjB8S/150-150-1.png[/imgalign][imgalign=left]https://i.ibb.co/9G2qdtZ/150x150-PSD-1.png[/imgalign][right][size=90][b]INTERNAL AFFAIRS BUREAU<br>\
+  LOS SANTOS COUNTY SHERIFF`S DEPARTMENT<br>\
+  PROFESSIONAL STANDARTS DIVISION[/b]<br>\
+  200 MСDONALD STREET<br>\
+  HALL OF JUSTICE<br>\
+  LOS SANTOS, SA<br>\
+  TEL.: +1 212 8934371<br>\
+  FAX: +1 212 8933165<br>\
+  [b][u][i]lssd.org[/box][/i][/u][/b][/size][hr][/hr]<br>\
+  <br>\
+  <br>\
+  [i][b]от:[/b] следователя "+document.getElementById('sledovatelName').value+" "+document.getElementById('sledovatelSurname').value+".<br>\
+  [b]кому:[/b] "+document.getElementById('officerRank').value+" "+document.getElementById('officerName').value+" "+document.getElementById('officerSurname').value+".[/i]<br>\
+  <br>\
+  <br>\
+  [center][b]УВЕДОМЛЕНИЕ[/b][/center]<br>\
+  <br>\
+  <br>\
+  <br>\
+  [justify]Уведомляем Вас о том, что Бюро Внутренних Расследований проводит разбирательство с вашим участием. Для получения дополнительной информации, свяжитесь с сержантом Бюро - Дэвидом Асеведой.[/justify]<br>\
+  <br>\
+  <br>\
+  [i]С уважением,<br>\
+  "+document.getElementById('sledovatelRank').value+" "+document.getElementById('sledovatelName').value+" "+document.getElementById('sledovatelSurname').value+"[/i] [float=right][i]"+document.getElementById('sign').value+"[/i][/float][/textarea]<br>\
+  </code>";
+}
